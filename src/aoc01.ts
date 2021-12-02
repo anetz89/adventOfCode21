1ff13b5c38cfdad1eada673cfe66fa188526dd4a
anetz89 <anetz@gmx.net>
2021-12-02 06:55:28 +0100

    finalized aoc01

--- src/aoc01.ts
+++ src/aoc01.ts
@@ -4,15 +4,10 @@ export function aoc(isPart2=false): any {
     let list = readNumberList('./assets/aoc01.txt');
 
     if (isPart2) {
-        list = list.map((elem, idx): number => {
-            if (list.length <= idx + 2) { return 0; }
-            return elem + list[idx + 1] + list[idx + 2];
-        });
+        list = list.map((elem, idx): number => elem + list[idx + 1] + list[idx + 2]);
     }
 
-    return list.map((elem, idx): number => {
-        if (idx < 0) { return 0; }
-        return (elem > list[idx - 1])? 1 : 0;
-    }).reduce((a: number, b: number) => a + b);
+    return list.map((elem, idx): number => (elem > list[idx - 1])? 1 : 0)
+               .reduce((a: number, b: number) => a + b);
 }
 
